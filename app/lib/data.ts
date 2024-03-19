0b4f8815fc1d3e9182c39537f30ea60ce348ce65
GuillermoDaniGarcia <163834885+GuillermoDaniGarcia@users.noreply.github.com>
2024-03-19 08:50:23 +0100

    fifth commit (capitulo 7)

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,6 @@
 import { sql } from '@vercel/postgres';
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
 import {
   CustomerField,
   CustomersTableType,
